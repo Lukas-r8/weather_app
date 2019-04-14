a0cb0edf716057ca4f4fc0cd72742ae30464bf76
Lucas Alves <lukas-r8@hotmail.com>
2019-04-14 21:09:45 +0200

    added new features

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,8 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
+const messageFour = document.querySelector('#message-4')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -12,6 +14,8 @@ weatherForm.addEventListener('submit', (e) => {
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
+    
+
 
     fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
